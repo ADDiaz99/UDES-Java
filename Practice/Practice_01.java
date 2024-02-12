87b1624c0a17bd6aa526363fda3df4f5b43b8e27
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2024-02-11 22:42:55 -0500

    added new practice exercise

---
+++ Practice/Practice_01.java
@@ -0,0 +1,23 @@
+package Practice;
+// import java.util.Scanner;
+
+public class Practice_01 {
+    
+    public static void main(String[] args){
+        // Scanner bring = new Scanner(System.in);
+        
+        // Variable manipulation
+        char m = 'M';
+        char a = 'A';
+        String phrase = "I love ";
+        int num = 94;
+        
+        System.out.println(phrase + m + m + a);
+        
+        for (int i = 1; i <= 10000; i++){
+            
+            System.out.println(num * i);
+        }
+    }
+    
+}
