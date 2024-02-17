9ce6698b024a890a90cabba9412384ca877a3cca
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2024-02-17 00:31:02 -0500

    added new exercise

---
+++ Hoja_de_Ejercicios_1/Ejercicio_01.java
@@ -0,0 +1,12 @@
+package Hoja_de_Ejercicios_1;
+import java.util.Scanner;
+/*
+ * Ana está confundida con el teorema de pitágoras y desea obtener el valor del cateto opuesto a partir de esté teorema.  
+ * Su tarea consiste en realizar una solución computacional algorítmica que permita obtener el cateto opuesto y mostrarselo a Ana.
+ * 
+ */
+public class Ejercicio_01 {
+    public static void main(String[] args){
+        Scanner bring = new Scanner(System.in);
+    }
+}
