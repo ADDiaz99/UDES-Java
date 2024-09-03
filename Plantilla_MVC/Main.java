5c4a630f6a450b7880c93e57bd93cd3383745f40
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2024-09-03 16:25:05 -0500

    added plantillamvc

--- Plantilla_MVC/Main.java
+++ Plantilla_MVC/Main.java
@@ -1,5 +1,10 @@
 package Plantilla_MVC;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Modelo modelo = new Modelo();
+        Vista vista = new Vista();
+        Controlador controlador = new Controlador(modelo, vista);
+        controlador.iniciar();
+    }
 }
